eafb7b5e2baa4eaba9f863667ed20f265649aba9
lan anh234lan anh 234 <nguyenthilananh21092005@gmail.com>
2025-02-06 10:46:45 +0700

    BT-SS5

---
+++ BAI4.sql
@@ -0,0 +1,40 @@
+create database Bai4ss5;
+use Bai4ss5;
+-- Tạo bảng products
+CREATE TABLE products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY, 
+    product_name VARCHAR(100) NOT NULL,        
+    category VARCHAR(50) NOT NULL,            
+    price DECIMAL(10, 2) NOT NULL,            
+    stock_quantity INT NOT NULL               
+);
+
+-- Thêm bản ghi vào products
+INSERT INTO products (product_name, category, price, stock_quantity)
+VALUES
+('Laptop Dell XPS 13', 'Electronics', 25999.99, 10),
+('Nike Air Max 270', 'Footwear', 4999.00, 50),
+('Samsung Galaxy S22', 'Electronics', 19999.99, 25),
+('T-Shirt Uniqlo', 'Clothing', 299.99, 100),
+('Apple AirPods Pro', 'Accessories', 5999.00, 15),
+('T-Shirt Apolo', 'Clothing', 199.99, 100);
+
+select product_name, category, price, stock_quantity
+from products
+order by price DESC
+limit 3;
+
+select product_name, category, price, stock_quantity
+from products p
+limit 2 offset 2;
+
+
+select product_name, category, price, stock_quantity 
+from products
+where category = 'Electronics'
+order by category DESC;
+
+select product_name, category, price, stock_quantity
+from products
+order by price ASC
+limit 1;
